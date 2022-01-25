311a5d6ddc93e43bd560e0c786f86d962c3af572
arturgarlacz <artur.garlacz@polcode.net>
2022-01-25 18:31:01 +0100

    Init repo

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    printWidth: 96,
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    tabWidth: 4,
+    arrowParens: 'avoid',
+    endOfLine: 'auto'
+};
---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: ['**/*.{ts,tsx}', '!**/node_modules/**', '!**/*.stories.tsx'],
+};
